f325487f7c3afe5604fabaa47db1cb181ab3d2b0
SultanaHabiba127 <suhabiba12@gmail.com>
2020-05-09 15:18:21 +0600

    some refactoring done

--- resources/views/pages/product/index.blade.php
+++ resources/views/pages/product/index.blade.php
@@ -12,7 +12,7 @@
           </div>
           <div class="col-md-8">
               <div class="widget">
-                  <h3>Featured Product</h3>
+                  <h3>Products</h3>
                   <div class="row">
                       <div class="col-md-3">
                           <div class="card">
@@ -24,76 +24,7 @@
                               </div>
                             </div>
                       </div>
-                      <div class="col-md-3">
-                          <div class="card">
-                          <img class="card-img-top feature-img" src="{{ asset('images/products/'. '1.jpg') }}" alt="Card image">
-                              <div class="card-body">
-                                <h4 class="card-title">Samsung</h4>
-                                <p class="card-text">Taka 10,000</p>
-                                <a href="#" class="btn btn-outline-warning">Add To Cart</a>
-                              </div>
-                            </div>
-                      </div>
-                      <div class="col-md-3">
-                          <div class="card">
-                          <img class="card-img-top feature-img" src="{{ asset('images/products/'. '1.jpg') }}" alt="Card image">
-                              <div class="card-body">
-                                <h4 class="card-title">Samsung</h4>
-                                <p class="card-text">Taka 10,000</p>
-                                <a href="#" class="btn btn-outline-warning">Add To Cart</a>
-                              </div>
-                            </div>
-                      </div>
-                      <div class="col-md-3">
-                          <div class="card">
-                          <img class="card-img-top feature-img" src="{{ asset('images/products/'. '1.jpg') }}" alt="Card image">
-                              <div class="card-body">
-                                <h4 class="card-title">Samsung</h4>
-                                <p class="card-text">Taka 10,000</p>
-                                <a href="#" class="btn btn-outline-warning">Add To Cart</a>
-                              </div>
-                            </div>
-                      </div>
-                      <div class="col-md-3">
-                          <div class="card">
-                          <img class="card-img-top feature-img" src="{{ asset('images/products/'. '1.jpg') }}" alt="Card image">
-                              <div class="card-body">
-                                <h4 class="card-title">Samsung</h4>
-                                <p class="card-text">Taka 10,000</p>
-                                <a href="#" class="btn btn-outline-warning">Add To Cart</a>
-                              </div>
-                            </div>
-                      </div>
-                      <div class="col-md-3">
-                          <div class="card">
-                          <img class="card-img-top feature-img" src="{{ asset('images/products/'. '1.jpg') }}" alt="Card image">
-                              <div class="card-body">
-                                <h4 class="card-title">Samsung</h4>
-                                <p class="card-text">Taka 10,000</p>
-                                <a href="#" class="btn btn-outline-warning">Add To Cart</a>
-                              </div>
-                            </div>
-                      </div>
-                      <div class="col-md-3">
-                          <div class="card">
-                          <img class="card-img-top feature-img" src="{{ asset('images/products/'. '1.jpg') }}" alt="Card image">
-                              <div class="card-body">
-                                <h4 class="card-title">Samsung</h4>
-                                <p class="card-text">Taka 10,000</p>
-                                <a href="#" class="btn btn-outline-warning">Add To Cart</a>
-                              </div>
-                            </div>
-                      </div>
-                      <div class="col-md-3">
-                          <div class="card">
-                          <img class="card-img-top feature-img" src="{{ asset('images/products/'. '1.jpg') }}" alt="Card image">
-                              <div class="card-body">
-                                <h4 class="card-title">Samsung</h4>
-                                <p class="card-text">Taka 10,000</p>
-                                <a href="#" class="btn btn-outline-warning">Add To Cart</a>
-                              </div>
-                            </div>
-                      </div>
+                      
                   </div>
               </div>
           </div>
